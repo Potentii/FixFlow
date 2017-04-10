e88f691b1f8eefbaf8a7f7c20428b7f934fa9f29
Guilherme Reginaldo Ruella <potentii@gmail.com>
2017-04-10 20:06:47 -0300

    Platform changed
    - The chosen platform has been changed (from Java to NodeJS)

--- src/main/java/com/potentii/fixflow/entities/sets/ETicketStatus.java
+++
@@ -1,5 +0,0 @@
-package com.potentii.fixflow.entities.sets;
-
-public enum ETicketStatus {
-	PENDING, SOLVING, CLOSED
-}
--- src/main/java/com/potentii/fixflow/entities/sets/ETicketUrgency.java
+++
@@ -1,5 +0,0 @@
-package com.potentii.fixflow.entities.sets;
-
-public enum ETicketUrgency {
-	LOW, HIGH, CRITICAL
-}
