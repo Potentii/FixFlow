ba42db901848fa89f557f3d282c98daae0c55fbf
Guilherme Reginaldo Ruella <potentii@gmail.com>
2017-04-17 21:58:36 -0300

    UI done
    - Feedback creation page done

---
+++ static/pages/give-feedback/give-feedback.js
@@ -0,0 +1,142 @@
+pages.add('give-feedback', '/ticket/:id/give-feedback', {
+
+   components: {
+      'custom-header': ui.get('custom-header'),
+      'custom-footer': ui.get('custom-footer')
+   },
+
+   data(){
+      return {
+         ticket_fk: undefined,
+
+         rating: undefined,
+         resolved: 'true',
+         message: undefined,
+
+         rating_values: ['Excelent', 'Good', 'Regular', 'Bad', 'Very bad']
+      };
+   },
+
+   mounted(){
+      this.ticket_fk = this.$route.params.id;
+   },
+
+   methods: {
+
+      submit(){
+         // *Converting the selected rating to a 0-4 number:
+         const rating = this.rating_values
+            .reverse()
+            .indexOf(this.rating);
+
+         // *Building the data to be sent:
+         const data = {
+            rating: (rating>-1) ? rating : undefined,
+            resolved: (this.resolved==='true') ? true : false,
+            message: this.message
+         };
+
+         // *Trying to create the ticket:
+         fetch('/api/v1/clients/tickets/' + this.ticket_fk + '/feedback', {
+            method: 'POST',
+            headers: new HeadersBuilder()
+               .addAccess()
+               .addJSONBody()
+               .get(),
+            body: JSON.stringify(data)
+         })
+         .then(res => {
+            // *Checking the response status:
+            switch(res.status){
+               case 201:
+                  // *If everything went fine:
+                  // *Parsing the response body:
+                  return res.json()
+                     .then(info => {
+                        // TODO show a snack
+                        // *Sending the user to the feedback view page:
+                        this.$router.push('/ticket/' + this.ticket_fk + '/feedback');
+                     });
+               case 400:
+                  // *If some input was incorrect:
+                  // TODO show a snack
+                  // *Throwing an error:
+                  throw new Error('invalid data');
+               default:
+                  // *If other error happened:
+                  // TODO show a snack
+                  // *Throwing an error:
+                  throw new Error('server error');
+            }
+         })
+         // *Logging errors:
+         .catch(err => (ENV!=ENVS.PROD) && console.error(err));
+      }
+
+   },
+
+   template:
+      `
+      <div class="give-feedback-page content-page">
+
+         <custom-header title="Feedback" subtitle="about the ticket"></custom-header>
+
+         <div class="content-wrapper">
+            <form id="give-feedback-page-form" class="content" @submit.prevent="submit">
+
+               <div class="give-feedback-page-form-rating-group radio-group">
+                  <label>Has the ticket been solved?</label>
+                  <div class="radio-group-options">
+                     <label class="mdc-radio" data-mdc-auto-init="MDCRadio">
+                        <input class="mdc-radio__native-control" type="radio" value="true" v-model="resolved" name="resolved-radio" checked>
+                        <div class="mdc-radio__background">
+                           <div class="mdc-radio__outer-circle"></div>
+                           <div class="mdc-radio__inner-circle"></div>
+                        </div>
+                        <span class="mdc-radio__label">Yes</span>
+                     </label>
+
+                     <label class="mdc-radio" data-mdc-auto-init="MDCRadio">
+                        <input class="mdc-radio__native-control" type="radio" value="false" v-model="resolved" name="resolved-radio">
+                        <div class="mdc-radio__background">
+                           <div class="mdc-radio__outer-circle"></div>
+                           <div class="mdc-radio__inner-circle"></div>
+                        </div>
+                        <span class="mdc-radio__label">No</span>
+                     </label>
+                  </div>
+               </div>
+
+               <div class="radio-group">
+                  <label>How do you describe the service quality?</label>
+                  <div class="radio-group-options">
+                     <div class="give-feedback-page-form-rating mdc-select" role="listbox" tabindex="0" data-mdc-auto-init="MDCSelect" required>
+                        <span class="mdc-select__selected-text"></span>
+                        <div class="mdc-simple-menu mdc-select__menu" data-mdc-auto-init="MDCSimpleMenu">
+                           <ul class="mdc-list mdc-simple-menu__items">
+                              <li class="mdc-list-item" role="option" tabindex="0" @click="rating = rv" v-for="rv in rating_values">
+                                 {{ rv }}
+                              </li>
+                           </ul>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+
+               <label class="give-feedback-page-form-message mdc-textfield mdc-textfield--multiline mdc-textfield--fullwidth" data-mdc-auto-init="MDCTextfield">
+                  <textarea class="mdc-textfield__input" rows="3" v-model="message"></textarea>
+                  <span class="mdc-textfield__label">If you want to add something else, we'd love to hear more from you</span>
+               </label>
+
+            </form>
+         </div>
+
+         <custom-footer>
+            <button type="submit" form="give-feedback-page-form" class="mdc-button mdc-button--raised mdc-button--accent" data-mdc-auto-init="MDCRipple">
+               send
+            </button>
+         </custom-footer>
+
+      </div>
+      `
+});
